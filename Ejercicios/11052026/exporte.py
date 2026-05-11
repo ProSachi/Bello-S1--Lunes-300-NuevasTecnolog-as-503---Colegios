dd6a1fc3f890e9bc5f05eae45ad998726c603c9b
Prosachi <syosa@cesde.net>
2026-05-11 17:26:05 -0500

    semana15

--- Ejercicios/11052026/exporte.py
+++ Ejercicios/11052026/exporte.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
 from cargaApi import obtener_datos_enfermeros
-
 # Obtener los datos desde la API
 df = obtener_datos_enfermeros()
-
 if df is not None:
     print("Datos cargados exitosamente:")
     print(df.head())
     print(f"\nColumnas disponibles: {df.columns.tolist()}")
     print(f"Total de registros: {len(df)}")
-
 # 1. Creamos un gráfico de prueba
-
 plt.figure(figsize=(10, 8))
 colores=['blue','orange','green']
 plt.barh(df['name'], df['stock'], color=colores, edgecolor='black', linewidth=0.7)
 plt.xlabel('Stock (Unidades)', fontsize=12, fontweight='bold')
-plt.ylabel('Productos', fontsize=12, fontweight='bold')
+plt.ylabel('Unidades', fontsize=12, fontweight='bold')
 plt.title("Reporte de Producción")
-plt.ylabel("Unidades")
 plt.xticks(rotation=45)
+
+plt.savefig("data/reporte_impresion.png", format='png', dpi=300, bbox_inches='tight')
+plt.savefig("data/reporte_web.svg", format='svg', bbox_inches='tight')
+plt.savefig("data/reporte_ejecutivo.pdf", format='pdf', bbox_inches='tight')
+plt.show()
 
 
 # ==========================================
@@ -35,21 +35,21 @@ plt.xticks(rotation=45)
 # Ideal para: PowerPoint, Word, correos electrónicos o impresión física.
 # Parámetro técnico: dpi=300 (Puntos por pulgada). Es el estándar internacional 
 # para calidad de imprenta. Si omites el DPI, se guardará a 72 o 100 DPI (se verá borroso).
-plt.savefig("reporte_impresion.png", format='png', dpi=300, bbox_inches='tight')
+#plt.savefig("data/reporte_impresion.png", format='png', dpi=300, bbox_inches='tight')
 
 # Opción B: SVG (Vectorial / Matemático)
 # Ideal para: Páginas web, aplicaciones interactivas o diseño gráfico (Illustrator/Figma).
 # No usa DPI porque no está hecho de píxeles, sino de fórmulas matemáticas. 
 # Puedes hacerle zoom infinito sin que se pixele.
-plt.savefig("reporte_web.svg", format='svg', bbox_inches='tight')
+#plt.savefig("data/reporte_web.svg", format='svg', bbox_inches='tight')
 
 # Opción C: PDF (Vectorial Empaquetado)
 # Ideal para: Informes formales, adjuntos gerenciales o entregables académicos.
 # Al igual que el SVG, el PDF guarda los gráficos de forma vectorial, garantizando
 # texto nítido sin importar cuánto zoom haga el gerente.
-plt.savefig("reporte_ejecutivo.pdf", format='pdf', bbox_inches='tight')
+#plt.savefig("data/reporte_ejecutivo.pdf", format='pdf', bbox_inches='tight')
 
 # ==========================================
 # RENDERIZADO FINAL
 # ==========================================
-plt.show() # Ahora sí, mostramos en pantalla y vaciamos la memoria.
+#plt.show() # Ahora sí, mostramos en pantalla y vaciamos la memoria.
